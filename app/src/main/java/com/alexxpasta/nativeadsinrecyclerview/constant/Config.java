336a124643376ea1bfb5d56008766ed14b40b955
alexpasta <alex761012@gmail.com>
2017-04-17 18:57:17 +0800

    [Doc] Add more info for README.md

--- app/src/main/java/com/alexxpasta/nativeadsinrecyclerview/constant/Config.java
+++ app/src/main/java/com/alexxpasta/nativeadsinrecyclerview/constant/Config.java
@@ -7,7 +7,7 @@ package com.alexxpasta.nativeadsinrecyclerview.constant;
 public class Config {
     public static final int MIN_AD_POSITION = 2;
     public static final int MAX_AD_POSITION = 25;
-    public static final int MIN_DISTANCE_BETWEEN_ADS = 7; // start from 1, which means adjacent
+    public static final int MIN_DISTANCE_BETWEEN_ADS = 5; // start from 1, which means adjacent
     public static final int FAN_POOL_SIZE = 2;
 //    public static final int MOPUB_POOL_SIZE = 1;
 }
